1321cd5bb172b95b4ace49b9a0a52a8eed3c1245
Tomek Banach <tomek@banciur.org>
2019-02-27 23:59:15 +0100

    Working ledger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+import TransportU2F from "@ledgerhq/hw-transport-u2f";
+import Eth from "@ledgerhq/hw-app-eth";
+
+const fun = async function() {
+     const transport = await TransportU2F.create();
+     const eth = new Eth(transport);
+     const address = await eth.getAddress("44'/60'/0'/0/0");
+     console.log("address", address)
+};
+console.log("hello waiting for transport");
+fun();
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,22 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: ["@babel/polyfill","./src/index.js"],
     plugins: [
         new CleanWebpackPlugin(["dist"]),
         new HtmlWebpackPlugin(),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
